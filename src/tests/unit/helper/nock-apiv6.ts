810ca8a749230cc41e9d176ea2048b8b02f4366b
galta <48960890+galta95@users.noreply.github.com>
2020-11-25 14:16:21 +0200

    feat(/api/0.6/changeset/{id}/close): added unit tests

--- src/tests/unit/helper/nock-apiv6.ts
+++ src/tests/unit/helper/nock-apiv6.ts
@@ -1,6 +1,6 @@
 import nock = require('nock');
 import { testConf } from '../config/tests-config';
-import { createChangesetEndPoint } from '../../../lib/endpoints';
+import { createChangesetEndPoint, closeChangesetEndPoint } from '../../../lib/constants/endpoints';
 
 const { host, port } = testConf;
 
@@ -14,4 +14,17 @@ export const createChangesetNock = (): void => {
     nock(`${host}:${port}`)
     .put(`${createChangesetEndPoint}`)
     .reply(200, '12');
+};
+
+export const closeChangesetNock = {
+    sanity: (id: number): void => {
+        nock(`${host}:${port}`)
+        .put(`${closeChangesetEndPoint(id)}`)
+        .reply(200, `changeset ${id} has closed`);
+    },
+    closedChangeset: (id: number): void => {
+        nock(`${host}:${port}`)
+        .put(`${closeChangesetEndPoint(id)}`)
+        .reply(409, `The changeset ${id} was closed at 2020-11-25 09:35:13 UTC`);
+    }
 };
--- src/tests/unit/unit.test.ts
+++ src/tests/unit/unit.test.ts
@@ -3,11 +3,11 @@ import { describe, it } from 'mocha';
 
 import Apiv6 from '../../../index';
 import { testConf } from './config/tests-config';
-import { createChangesetNock, createChangesetNockNotAuth } from './helper/nock-apiv6';
+import { createChangesetNock, createChangesetNockNotAuth, closeChangesetNock } from './helper/nock-apiv6';
 
 const { host, port, username, password } = testConf;
 
-describe('apiv6', async function () { 
+describe('apiv6', async function () {
     const apiv6 = new Apiv6(host, port, username, password);
     describe('happy flow', async function () {
         describe('/changeset/create', async function () {
@@ -36,6 +36,37 @@ describe('apiv6', async function () {
                                         .and.to.be.equal(401);
                     expect(res).to.have.property('message')
                             .and.to.be.equal("Couldn't authenticate you");
+                });
+            });
+        });
+        describe('/changeset/{id}/close}', async function () {
+            describe('/changeset/create', async function () {
+                const changeSetNumber = 12;
+                describe('senity check', async function () {
+                    before(async function () {
+                        closeChangesetNock.sanity(changeSetNumber);
+                    });
+                    it('should close the changset', async function () {
+                        const res = await apiv6.closeChangeset(changeSetNumber);
+                        expect(res).to.be.a('object')
+                                            .with.property('code')
+                                            .and.to.be.equal(200);
+                        expect(res).to.have.property('message')
+                                .and.to.be.equal(`changeset ${changeSetNumber} has closed`);
+                    });
+                });
+                describe('with already closed changest', async function () {
+                    before(async function () {
+                        closeChangesetNock.closedChangeset(changeSetNumber);
+                    });
+                    it('should get error message', async function () {
+                        const res = await apiv6.closeChangeset(Number(changeSetNumber));
+                        expect(res).to.be.a('object')
+                                            .with.property('code')
+                                            .and.to.be.equal(409);
+                        expect(res).to.have.property('message')
+                                .and.to.be.equal(`The changeset ${changeSetNumber} was closed at 2020-11-25 09:35:13 UTC`);
+                    });
                 });
             });
         });
